310f5fb0c0e52cad91309bab746ea582a47cfce1
Sjur Martin Kleppan <sjurkleppan@gmail.com>
2020-02-20 19:07:54 +0100

    Initial commit

---
+++ src/main/java/com/MilleniumEvent/demo/MilleniumEventApplication.java
@@ -0,0 +1,13 @@
+package com.MilleniumEvent.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilleniumEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilleniumEventApplication.class, args);
+    }
+
+}
